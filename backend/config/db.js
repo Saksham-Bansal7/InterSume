b2bb6f85d9c74971e46015c975fc7408b767d026
Saksham Bansal <sakshambansalalka@gmail.com>
2025-07-14 18:50:45 +0530

    changed api keys, database Url and Jwt token

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+const dbURL = process.env.DB_CONNECTION_STRING ;
+
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://sakshambansalalka:resume123@cluster0.y4vumjc.mongodb.net/RESUME').then(() => {
+  await mongoose.connect(dbURL).then(() => {
     console.log("MongoDB connected successfully");
   }).catch((error) => {
     console.error("MongoDB connection failed:", error);
